af13b35babd329a74ee688906c5bb037f727914a
samikshyalamichhane <samikshya.lamichhane111@gmail.com>
2023-02-14 17:15:16 +0545

    updated

--- Modules/Frontend/Resources/views/approach.blade.php
+++ Modules/Frontend/Resources/views/approach.blade.php
@@ -27,95 +27,100 @@
 </section>
 
 <section class="upcoming-events-wrap section-padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-lg-12 text-center">
-                    <div class="event-cat-filter mb-20">
-                        <button data-filter="*" class="active">All</button>
-                        <button data-filter=".water">Our Approach</button>
-                        <button data-filter=".festival">Our Strategies</button>
-                                            </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-12 text-center">
+                <div class="event-cat-filter mb-20">
+                    <button data-filter="*" class="active">All</button>
+                    <button data-filter=".water">Our Approach</button>
+                    <button data-filter=".festival">Our Strategies</button>
                 </div>
             </div>
-
-            <div class="row grid" style="position: relative; height: 1025px;">
-                                <div class="col-12 col-lg-6 col-xl-12 water proevent grid-item" style="position: absolute; left: 0%; top: 0px;">
-                    <div class="single-event-ticket row align-items-center">
-                        <div class="col-xl-4">
-                            <div class="event-featured-cover bg-cover" style="background-image: url('https://caed.webhousejapan.com//storage/public/partner/1671102559.jpg')">
-                                <div class="event-time-address">
-                                    <span><i class="fal fa-calendar-alt"></i>24th June 2020</span>
-                                    <!-- <span><i class="fal fa-map-marker-alt"></i>NY, London</span> -->
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-5 pr-xl-0">
-                            <div class="event-info">
-                                <h2><a href="event-details.html">Project Management In The Voluntary Sector</a></h2>
-                                <p></p><h2 style="margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px; line-height: 2.5rem; font-size: 1.875rem; border: 0px; outline: 0px; font-family: arialregular; color: rgb(32, 36, 38);"><span style="color: rgb(114, 116, 117); font-family: Roboto, sans-serif; font-size: 18px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid unt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repreh enderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</span></h2><p></p>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 text-xl-right">
-                            <div class="event-ticket-buy">
-                                <a href="event-details.html" class="ticket-buy-btn"><i class="fal fa-home"></i>Book A Seat</a>
-                            </div>
-                        </div>
-                    </div> <!-- single-event-ticket -->
-                </div>  
-                 
-                 
-                <div class="col-12 col-lg-6 col-xl-12 festival proevent grid-item" style="position: absolute; left: 0%; top: 434px;">
-                    <div class="single-event-ticket row align-items-center">
-                        <div class="col-xl-4">
-                            <div class="event-featured-cover bg-cover" style="background-image: url('https://caed.webhousejapan.com//storage/public/partner/1671102589.png')">
-                                <div class="event-time-address">
-                                    <span><i class="fal fa-calendar-alt"></i>24th July 2021</span>
-                                    <!-- <span><i class="fal fa-map-marker-alt"></i>NY, London</span> -->
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-5 pr-xl-0">
-                            <div class="event-info">
-                                <h2><a href="event-details.html">10 Ways To Help Others That Will Lead You</a></h2>
-                                <p></p><p><span style="color: rgb(114, 116, 117); font-family: Roboto, sans-serif; font-size: 18px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid unt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repreh enderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</span></p><p></p>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 text-xl-right">
-                            <div class="event-ticket-buy">
-                                <a href="event-details.html" class="ticket-buy-btn"><i class="fal fa-home"></i>Book A Seat</a>
-                            </div>
-                        </div>
-                    </div> <!-- single-event-ticket -->
-                </div> 
-                  
-                 
-                <div class="col-12 col-lg-6 col-xl-12 implementing proevent grid-item" style="position: absolute; left: 0%; top: 755px;">
-                    <div class="single-event-ticket row align-items-center">
-                        <div class="col-xl-4">
-                            <div class="event-featured-cover bg-cover" style="background-image: url('https://caed.webhousejapan.com//storage/public/partner/1671102625.png')">
-                                <div class="event-time-address">
-                                    <span><i class="fal fa-calendar-alt"></i>24th July 2021</span>
-                                    <!-- <span><i class="fal fa-map-marker-alt"></i>NY, London</span> -->
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-5 pr-xl-0">
-                            <div class="event-info">
-                                <h2><a href="event-details.html">Helping The Church To Serve Others</a></h2>
-                                <p></p><p><span style="color: rgb(114, 116, 117); font-family: Roboto, sans-serif; font-size: 18px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid unt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repreh enderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</span></p><p></p>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 text-xl-right">
-                            <div class="event-ticket-buy">
-                                <a href="event-details.html" class="ticket-buy-btn"><i class="fal fa-home"></i>Book A Seat</a>
-                            </div>
-                        </div>
-                    </div> <!-- single-event-ticket -->
-                </div> 
-                         
-            </div>
-
-            
         </div>
-    </section>
+
+        <div class="row grid" style="position: relative; height: 1025px;">
+            @foreach($approaches->where('category','approaches') as $project)
+            <div class="col-12 col-lg-6 col-xl-12 water proevent grid-item" style="position: absolute; left: 0%; top: 0px;">
+                <div class="single-event-ticket row align-items-center">
+                    <div class="col-xl-4">
+                        <div class="event-featured-cover bg-cover" style="background-image: url('{{Storage::url($project->image)}}')">
+                            <div class="event-time-address">
+                                <span><i class="fal fa-calendar-alt"></i>{{ \Carbon\Carbon::parse($project->created_at)->format('jS F, Y') }}</span>
+                                <!-- <span><i class="fal fa-map-marker-alt"></i>NY, London</span> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-5 pr-xl-0">
+                        <div class="event-info">
+                            <h2><a href="event-details.html">{{$project->title}}</a></h2>
+                            <p>{!! $project->description !!}</p>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 text-xl-right">
+                        <div class="event-ticket-buy">
+                            <a href="event-details.html" class="ticket-buy-btn"><i class="fal fa-home"></i>Book A Seat</a>
+                        </div>
+                    </div>
+                </div> <!-- single-event-ticket -->
+            </div>
+            @endforeach
+
+            @foreach($approaches->where('category','strategies') as $project)
+            <div class="col-12 col-lg-6 col-xl-12 festival proevent grid-item" style="position: absolute; left: 0%; top: 434px;">
+                <div class="single-event-ticket row align-items-center">
+                    <div class="col-xl-4">
+                        <div class="event-featured-cover bg-cover" style="background-image: url('{{Storage::url($project->image)}}')">
+                            <div class="event-time-address">
+                                <span><i class="fal fa-calendar-alt"></i>{{ \Carbon\Carbon::parse($project->created_at)->format('jS F, Y') }}</span>
+                                <!-- <span><i class="fal fa-map-marker-alt"></i>NY, London</span> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-5 pr-xl-0">
+                        <div class="event-info">
+                            <h2><a href="event-details.html">{{$project->title}}</a></h2>
+                            <p>{!! $project->description !!}</p>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 text-xl-right">
+                        <div class="event-ticket-buy">
+                            <a href="event-details.html" class="ticket-buy-btn"><i class="fal fa-home"></i>Book A Seat</a>
+                        </div>
+                    </div>
+                </div> <!-- single-event-ticket -->
+            </div>
+            @endforeach
+
+
+            {{--<div class="col-12 col-lg-6 col-xl-12 implementing proevent grid-item" style="position: absolute; left: 0%; top: 755px;">
+                <div class="single-event-ticket row align-items-center">
+                    <div class="col-xl-4">
+                        <div class="event-featured-cover bg-cover" style="background-image: url('https://caed.webhousejapan.com//storage/public/partner/1671102625.png')">
+                            <div class="event-time-address">
+                                <span><i class="fal fa-calendar-alt"></i>24th July 2021</span>
+                                <!-- <span><i class="fal fa-map-marker-alt"></i>NY, London</span> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-5 pr-xl-0">
+                        <div class="event-info">
+                            <h2><a href="event-details.html">Helping The Church To Serve Others</a></h2>
+                            <p></p>
+                            <p><span style="color: rgb(114, 116, 117); font-family: Roboto, sans-serif; font-size: 18px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid unt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repreh enderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</span></p>
+                            <p></p>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 text-xl-right">
+                        <div class="event-ticket-buy">
+                            <a href="event-details.html" class="ticket-buy-btn"><i class="fal fa-home"></i>Book A Seat</a>
+                        </div>
+                    </div>
+                </div> <!-- single-event-ticket -->
+            </div>--}}
+
+        </div>
+
+
+    </div>
+</section>
 @endsection
--- Modules/Frontend/Resources/views/projectinner.blade.php
+++ Modules/Frontend/Resources/views/projectinner.blade.php
@@ -41,76 +41,24 @@
 
 
                 </div>
-          <!--      <div id="accordion" class="mt-3 fffaq-section">-->
-          <!--          <h3>Our Partners</h3>-->
-          <!--  <div class="card">-->
-          <!--    <div class="card-header" id="heading-1">-->
-          <!--      <h5 class="mb-0">-->
-          <!--        <a role="button" data-toggle="collapse" href="#collapse-1" aria-expanded="true" aria-controls="collapse-1">-->
-          <!--                            International Partner-->
-          <!--                          </a>-->
-          <!--      </h5>-->
-          <!--    </div>-->
-          <!--    <div id="collapse-1" class="collapse show" data-parent="#accordion" aria-labelledby="heading-1">-->
-          <!--      <div class="card-body">-->
-          <!--        FXCOPIER is the worlds most reliable remote trade copier. Its allow you to copy trades almost instantly between hundreds of MT4 accounts through the use of technology. Many of the industries leading money managers use FXCopier to easily manage multiple-->
-          <!--        client accounts in tandem.-->
-          <!--      </div>-->
-          <!--    </div>-->
-          <!--  </div>-->
-          <!--  <div class="card">-->
-          <!--    <div class="card-header" id="heading-2">-->
-          <!--      <h5 class="mb-0">-->
-          <!--        <a class="collapsed" role="button" data-toggle="collapse" href="#collapse-2" aria-expanded="false" aria-controls="collapse-2">-->
-          <!--                            National Partner-->
-          <!--                          </a>-->
-          <!--      </h5>-->
-          <!--    </div>-->
-          <!--    <div id="collapse-2" class="collapse" data-parent="#accordion" aria-labelledby="heading-2">-->
-          <!--      <div class="card-body">-->
-          <!--        After subscription you will get our special trade copier. If you want to use this copier for business purpose or other commercial pupose then directly contact with www.fxcopier.co.uk.-->
-          <!--      </div>-->
-          <!--    </div>-->
-          <!--  </div>-->
-            
-          <!--</div>-->
-          <div id="accordion" class="accordion mt-3">
-              <h3>Our Partners</h3>
-        <div class="card mb-0">
-            <div class="card-header collapsed" data-toggle="collapse" href="#collapseOne">
-                <a class="card-title">
-                    International Partners
-                </a>
-            </div>
-            <div id="collapseOne" class="card-body collapse show" data-parent="#accordion" >
-                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                    aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat
-                    craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                </p>
-            </div>
-            <div class="card-header collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-                <a class="card-title">
-                  National Partners
-                </a>
-            </div>
-            <div id="collapseTwo" class="card-body collapse" data-parent="#accordion" >
-                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                    aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat
-                    craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                </p>
-            </div>
-            <div class="card-header collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-                <a class="card-title">
-                  International Partners
-                </a>
-            </div>
-            <div id="collapseThree" class="collapse" data-parent="#accordion" >
-                <div class="card-body">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                    aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. samus labore sustainable VHS.
+                <div id="accordion" class="accordion mt-3">
+                    <h3>Our Partners</h3>
+                    <div class="card mb-0">
+                        @foreach($project->projectPartners->where('publish',1) as $key=>$partner)
+                        <div class="card-header collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse{{$key}}">
+                            <a class="card-title">
+                                {{$partner->title}}
+                            </a>
+                        </div>
+                        <div id="collapse{{$key}}" class="card-body collapse" data-parent="#accordion">
+                            <p>
+                                {!! $partner->description !!}
+                            </p>
+                        </div>
+                        @endforeach
+                        
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
             </div>
             <div class="col-12 col-lg-4">
                 <div class="main-sidebar">
@@ -138,7 +86,7 @@
 </section>
 <section class="part-coll">
     <div class="container">
-        
+
     </div>
 </section>
 @endsection
